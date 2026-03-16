d13d783424e4dbedf456ba5ac39a2ed106da1f26
0ld-eugene <yudin.ev89@gmail.com>
2026-03-16 16:16:28 +0300

    fix

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const galleryContainer = document.querySelector('#gallery-container');
+window.onload = () => {
+   const savedSearch = localStorage.getItem('lastSearch');
+   if (savedSearch) {
+      searchInput.value = savedSearch; // Подставляем в инпут
+      getPhotos(savedSearch); // Сразу загружаем фото
+   }
+};
 
 async function getPhotos(query = 'pop') {
 
    try {
       const response = await fetch(`https://api.unsplash.com/search/photos?query=${query}&per_page=20&client_id=cHZwfh4KxU4_CRnsDDftgzrmXJg-yQwXnF_ALdtC0_I`)
       const data = await response.json();
+
+      if (data.results.length === 0) {
+         galleryContainer.innerHTML = 'По вашему запросу ничего не найдено';
+         return; // Выходим из функции, чтобы не запускать отрисовку пустых данных
+      }
+
       if (!response.ok) throw new Error("Сервер недоступен");
 
       let cardContent = '';
@@ -27,6 +40,7 @@ async function getPhotos(query = 'pop') {
          stagger: 0.2,     // Задержка между появлением каждой следующей карточки
          ease: "power4.out" // Плавное замедление в конце
       });
+      localStorage.setItem('lastSearch', query);
 
    } catch (error) {
       console.log("Ошибка загрузки сервера: ", error);
